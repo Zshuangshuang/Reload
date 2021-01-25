b943ae4b4be5d33e31ce1373e3b95dcda3931feb
Zshuangshuang <Zshuangshuang@yeah.net>
2021-01-25 22:46:11 +0800

    提交代码

---
+++ 2021_01_25OJ习题/src/Solution.java
@@ -0,0 +1,14 @@
+
+
+/**
+ * Author:ZouDouble
+ * Description:
+ * 天气：晴天
+ * 目标：Good Offer
+ * Date    2021-01-25 22:44
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("测试Git");
+    }
+}
