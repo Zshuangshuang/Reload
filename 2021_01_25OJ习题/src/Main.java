9d2e3dd09b7cde8ffea62fd04586ee92933bf779
Zshuangshuang <19990306Zss>
2021-01-25 22:07:47 +0800

    提交代码

---
+++ 2021_01_25OJ习题/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Author:ZouDouble
+ * Description:
+ * 天气：晴天
+ * 目标：Good Offer
+ * Date    2021-01-25 21:34
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
