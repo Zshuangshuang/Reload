079491af87841a6230621c7037beb5a0bfd8c4b8
Zshuangshuang <Zshuangshuang@yeah.net>
2021-01-30 22:28:25 +0800

    提交代码

--- 剑指Offer习题/src/com/ssz/从尾到头打印链表/Solution.java
+++ 剑指Offer习题/src/com/ssz/从尾到头打印链表/Solution.java
@@ -1,11 +1,28 @@
 package com.ssz.从尾到头打印链表;
 
-/**
- * Author:ZouDouble
- * Description:
- * 天气：晴天
- * 目标：Good Offer
- * Date    2021-01-29 13:46
- */
+import java.util.ArrayList;
+import java.util.Stack;
+
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
 public class Solution {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+      ArrayList list = new ArrayList();
+      Stack stack = new Stack();
+      while(listNode != null){
+          stack.push(listNode.val);
+          listNode = listNode.next;
+      }
+      while(!stack.isEmpty()){
+          list.add(stack.pop());
+      }
+      return list;
+    }
 }
