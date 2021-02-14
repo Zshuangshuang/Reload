6909a6261f6b1412142717d32f5a60f2101aad34
Zshuangshuang <1434278632@qq.com>
2021-02-14 22:42:51 +0800

    提交代码

---
+++ 2021_01_20/src/Solution2.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+       /* //方法1：(1)直接将nums2数组中的元素插入nums1
+        for (int i = 0; i < n; i++) {
+            nums1[m+i] = nums2[i];
+        }
+        //(2)排序后对整个数组进行排序
+        Arrays.sort(nums1);*/
+        //
+
+    }
+
+
+}
---
+++ 2021_01_20/src/TestDemo.java
@@ -0,0 +1,11 @@
+package PACKAGE_NAME;
+
+/**
+ * Author:ZouDouble
+ * Description:
+ * 天气：晴天
+ * 目标：Good Offer
+ * Date    2021-01-20 15:30
+ */
+public class TestDemo {
+}
